b8926af501eddc44a3e24158d18907f6db96c386
teresa881016 <teresa881016@gmail.com>
2023-08-28 20:27:22 +0900

    init commit

---
+++ src/main/java/assignment/firstpost/FirstpostApplication.java
@@ -0,0 +1,13 @@
+package assignment.firstpost;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstpostApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstpostApplication.class, args);
+	}
+
+}
---
+++ src/test/java/assignment/firstpost/FirstpostApplicationTests.java
@@ -0,0 +1,13 @@
+package assignment.firstpost;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstpostApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
